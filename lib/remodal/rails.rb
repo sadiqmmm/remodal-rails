1dbcc95c0566a789d810b46868ca1190cd69d5d2
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-10-25 02:04:36 +0530

    Initial commit

---
+++ lib/remodal/rails.rb
@@ -0,0 +1,7 @@
+require "remodal/rails/version"
+
+module Remodal
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/remodal/rails/version.rb
@@ -0,0 +1,5 @@
+module Remodal
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
